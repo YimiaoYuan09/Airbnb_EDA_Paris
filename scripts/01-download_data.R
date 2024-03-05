71a92f9be1c7c41099efbebb5144c3e8b45732e7
Yimiao Yuan <yymlinda.yuan@mail.utoronto.ca>
2024-03-04 21:07:10 -0500

    finish EDA

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,31 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the Airbnb listings data in Paris from Inside Airbnb
+# Author: Yimiao Yuan
+# Date: 04 March 2024
+# Contact: yymlinda.yuan@mail.utoronto.ca
+# License: --
+# Pre-requisites: --
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+url <-
+  paste0(
+    "http://data.insideairbnb.com/france/ile-de-france/",
+    "paris/2023-12-12/data/listings.csv.gz"
+  )
 
+airbnb_raw <-
+  read_csv(
+    file = url,
+    guess_max = 20000
+  )
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(airbnb_raw, "inputs/data/airbnb_raw_data.csv")
 
          
